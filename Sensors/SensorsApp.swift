8d575e2f19a406e88f6a0ff231938187f4aea9c7
Etienne Vautherin <etienne@vautherin.com>
2024-02-20 16:31:26 +0100

    Initial Commit

---
+++ Sensors/SensorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorsApp.swift
+//  Sensors
+//
+//  Created by Etienne Vautherin on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SensorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
